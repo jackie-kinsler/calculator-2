e8b972a43eeca5d137d01b547509c646248ff244
Jackie Kinsler <jackiekinsler@gmail.com>
2020-09-22 15:44:08 +0000

    Implemented all other arethmetic functions.

--- calculator.py
+++ calculator.py
@@ -11,6 +11,29 @@ user_input = input("")
 
 while user_input[0][0] != 'q' and user_input[0][0] != 'Q':
     tokens = user_input.split(" ")
-    if user_input[0] == '+':
+
+    if tokens[0] == '+':
         print(add(float(tokens[1]), float(tokens[2])))
+    
+    elif tokens[0] == '-':
+        print(subtract(float(tokens[1]), float(tokens[2])))
+    
+    elif tokens[0] == '*':
+        print(multiply(float(tokens[1]), float(tokens[2])))
+    
+    elif tokens[0] == '/':
+        print(divide(float(tokens[1]), float(tokens[2])))
+    
+    elif tokens[0] == 'square':
+        print(square(float(tokens[1])))
+    
+    elif tokens[0] == 'cube':
+        print(cube(float(tokens[1])))
+
+    elif tokens[0] == 'pow':
+        print(power(float(tokens[1]), float(tokens[2])))
+    
+    elif tokens[0] == 'mod':
+        print(mod(float(tokens[1]), float(tokens[2])))
+
     user_input = input("")
