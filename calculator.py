844e1a5e5639336ee1f05cdc327336f6597048e0
Jackie Kinsler <jackiekinsler@gmail.com>
2020-09-22 15:38:04 +0000

    Included add functionality.

--- calculator.py
+++ calculator.py
@@ -5,3 +5,12 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+
+user_input = input("")
+
+
+while user_input[0][0] != 'q' and user_input[0][0] != 'Q':
+    tokens = user_input.split(" ")
+    if user_input[0] == '+':
+        print(add(float(tokens[1]), float(tokens[2])))
+    user_input = input("")
